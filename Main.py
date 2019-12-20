6f0fff9962c9dafd959ec5726d90f1be0cbe9808
Samoth69 <thomasviolent@gmail.com>
2019-12-20 21:56:01 +0100

    it work !

--- Main.py
+++ Main.py
@@ -1,17 +1,19 @@
 # Main.py Violent Thomas 12/2019 PyCharm avec Python 3.8.0
+from random import randint
 
 import discord
 import json
 
 client = discord.Client()
 
-# nom du fichier contenant les dessins en ascii
+# name of the file with ascii drawing in it
 PATH_JSON_ASCII = "ascii.json"
 
-# nom du fichier contenant le token du bot
+# discord bot token id
 PATH_TXT_TOKEN = "token.txt"
 
 
+# Trigger when the bot is ready to work
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -20,6 +22,7 @@ async def on_ready():
     await client.change_presence(activity=ac, status=discord.Status.online, afk=False)
 
 
+# Trigger when their is a message to read
 @client.event
 async def on_message(message):
     try:
@@ -31,33 +34,39 @@ async def on_message(message):
 
         if message.content.startswith(';h'):
             await print_help(message)
+
+        if message.content.startswith(';a'):
+            await print_ascii_art(message, 1)
     except Exception:
         pass
 
 
-# Affiche l'aide du bot avec les commandes disponibles
-# Entrée: message - objet de type discord.Message (sert pour envoyer le message sur le bon salon)
+# Show the help with the commands that you can type
+# Input: message - Type: discord.Message (it's use to know where to put the message)
 async def print_help(message):
     emb = discord.Embed()
     emb.title = ":thinking: Help"
     emb.colour = discord.Colour.blue()
     # emb.description = "description"
     emb.add_field(name=";h or ;help", value="Show this help (list of commands)", inline=False)
-    emb.add_field(name=";a or ;ascii [s(mall)|m(edium)|l(arge)]",
-                  value="Print one ascii art thing, optionally, you can set the size of the print. If not mentioned, a random size will be chosen",
+    emb.add_field(name=";a or ;ascii [s(mall)|m(edium)|l(arge)]", value="Print one ascii art thing, optionally, you can set the size of the print. If not mentioned, a random size will be chosen",
                   inline=False)
     await message.channel.send(embed=emb)
 
 
-# écrit un "ascii art" dans le chat discord.
-# Entrées: message - objet de type discord.Message (sert pour envoyer le message sur le bon salon)
-#          size - integer - taille du dessin (0: non spécifié (random)
-#                                             1: petit
-#                                             2: moyen
-#                                             3: grand)
+# Print an "ASCII art" thing on discord chat
+# Input: message - Type: discord.Message (it's use to know where to put the message)
+#        size - integer - size of the drawing (in term of height) (0: not specified (random), 1: small, 2: medium, 3: large)
 async def print_ascii_art(message, size):
     with open(PATH_JSON_ASCII) as json_file:
         data = json.load(json_file)
+        if size == 1:
+            drawing_size = "small"
+            number_of_elements = data["ascii"][drawing_size][0]["size"]
+        rnd = randint(1, number_of_elements)
 
+        await message.channel.send("**" + data["ascii"][drawing_size][rnd]["name"] + "**\n```" + data["ascii"][drawing_size][rnd]["content"] + "```")
+
+# Load token file and then load the bot
 with open(PATH_TXT_TOKEN) as token_file:
     client.run(token_file.read())
